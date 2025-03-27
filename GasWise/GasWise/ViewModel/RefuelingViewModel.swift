f88f7d5886ec78adefc61fc358706be855542acb
Dor Mizrachi <155853102+Dor2495@users.noreply.github.com>
2025-03-27 11:14:49 +0200

    Update RefuelingViewModel.swift

--- GasWise/GasWise/ViewModel/RefuelingViewModel.swift
+++ GasWise/GasWise/ViewModel/RefuelingViewModel.swift
@@ -6,7 +6,12 @@ class RefuelingViewModel {
 
   var modelContext: ModelContext? = nil
 
-  
+  var allRefuelings: [Refueling] = []
 
+
+  func fetchRefuelings() {
+    let request = 
+    
+  }
 
 }
